e41dccdef0316e7d984c72498b4f0c4757af4f4b
meta-bay <bayiemeseret@gmail.com>
2023-12-17 19:20:28 +0300

    fix code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,8 +22,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = temp;
+				print_array(array, size);
 			}
 		}
-		print_array(array, size);
 	}
 }
